6eafe145db1b9c3795d59df3e98bae72522c64e7
Paulo Piovezan <paulorenatopiovezanfilho@gmail.com>
2026-09-16 19:23:35 -0300

    Added Comments so i remember what any of my own code does :/

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -6,18 +6,24 @@ const loginRouter = express.Router();
 
 
 loginRouter.post('/login', async (req,res) => {
-
+//Gets the username and password from the request body
 const username = req.body.username;
 const password = req.body.password;
 
+//Queries the DB to see if there is a user row matching the requested username and password
+
 const queryResponse = await db.query(`SELECT * FROM usuarios WHERE nomeusuario = $1 AND senha = $2`,[username,password]);
 queryData = queryResponse.rows;
-
+//If the length of the array returned from the query is 0, meaning there were no matching users found, the request fails
 if (queryData.length === 0){
 
     res.status(401).send("Credencias Incorretas!");
 
 }
+
+//If the length is NOT 0, meaning there was a match, sign a JWT that expires in 5 minutes 
+//and send it as a cookie to the frontend
+
 else{
 
     const token = jwt.sign({
@@ -27,6 +33,7 @@ else{
     }, process.env.JWT_SECRET, {expiresIn: '5m'})
 
     res.cookie('token',token,{httpOnly:true});
+//Upon succesful login, updates the matching user's refresh token and stores it in another cookie, also sent to the frontend, and sends a successful login response
     const reftokenResponse = await db.query(`UPDATE usuarios SET reftoken = gen_random_uuid() WHERE uid = $1 RETURNING reftoken`,[queryData[0].uid]);
     res.cookie('reftoken',reftokenResponse.rows[0].reftoken,{httpOnly:true});
     res.status(200).send("Login Efeituado com sucesso!");
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const loginRouter = require('./routes/login');
 const express = require('express');
-
+const jwtAuthCookie = require('./middleware/jwtAuthCookie');
 const app = express();
 const PORT = process.env.PORT;
 
 
 app.use(express.json());
+
 app.use('/auth', loginRouter);
 
-
+app.use('/api',jwtAuthCookie)
 
 app.listen(PORT, () => {console.log("App Listening on port " + PORT)})
